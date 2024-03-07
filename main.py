bb2f88ad57a6f21b34154f588e3410a8f5f592f1
CaturBS <ctrbudisantoso@gmail.com>
2024-03-07 11:14:09 +0700

    vicisession get

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ class ViciSess:
     vicisession = None
     @staticmethod
     def get_sesssion():
-        if ViciSess.vicisession is None:
-            ViciSess.vicisession = vici.Session()
+        # if ViciSess.vicisession is None:
+        ViciSess.vicisession = vici.Session()
         return ViciSess.vicisession
     
 @app.route('/')
