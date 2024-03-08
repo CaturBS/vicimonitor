2fd8845c50ab70ed5b5d3bffc58027c252d0c580
CaturBS <ctrbudisantoso@gmail.com>
2024-03-08 10:35:30 +0700

    data

--- main.py
+++ main.py
@@ -44,49 +44,49 @@ def connection_form():
     if request.method == 'POST' and form.validate():
         new_conn_params = OrderedDict()
         the_conn = OrderedDict()
-        new_conn_params[form.name] = the_conn
-        the_conn['version'] = form.version
-        the_conn['local_addrs'] = form.local_addrs
-        the_conn['remote_addrs'] = form.remote_addrs
-        the_conn['local_port'] = form.local_port
-        the_conn['remote_port'] = form.remote_port
-        the_conn['proposals'] = form.proposals
-        the_conn['vips'] = form.vips
-        the_conn['aggressive'] = form.aggressive
-        the_conn['pull'] = form.pull
-        if bool(form.dscp):
-            the_conn['dscp'] = form.dscp
-        the_conn['encap'] = form.encap
-        if bool(form.dpd_delay):
-            the_conn['dpd_delay'] = form.dpd_delay
-        if bool(form.dpd_timeout):
-            the_conn['dpd_timeout'] = form.dpd_timeout
-        the_conn['fragmentation'] = form.fragmentation
-        if bool(form.keyingtries):
-            the_conn['keyingtries'] = form.keyingtries
-        the_conn['unique'] = form.unique
-        if bool(form.reauth_time):
-            the_conn['reauth_time'] = form.reauth_time
-        if bool(form.rekey_time):
-            the_conn['rekey_time'] = form.rekey_time
+        new_conn_params[form.name.data] = the_conn
+        the_conn['version'] = form.version.data
+        the_conn['local_addrs'] = form.local_addrs.data
+        the_conn['remote_addrs'] = form.remote_addrs.data
+        the_conn['local_port'] = form.local_port.data
+        the_conn['remote_port'] = form.remote_port.data
+        the_conn['proposals'] = form.proposals.data
+        the_conn['vips'] = form.vips.data
+        the_conn['aggressive'] = form.aggressive.data
+        the_conn['pull'] = form.pull.data
+        if bool(form.dscp.data):
+            the_conn['dscp'] = form.dscp.data
+        the_conn['encap'] = form.encap.data
+        if bool(form.dpd_delay.data):
+            the_conn['dpd_delay'] = form.dpd_delay.data
+        if bool(form.dpd_timeout.data):
+            the_conn['dpd_timeout'] = form.dpd_timeout.data
+        the_conn['fragmentation'] = form.fragmentation.data
+        if bool(form.keyingtries.data):
+            the_conn['keyingtries'] = form.keyingtries.data
+        the_conn['unique'] = form.unique.data
+        if bool(form.reauth_time.data):
+            the_conn['reauth_time'] = form.reauth_time.data
+        if bool(form.rekey_time.data):
+            the_conn['rekey_time'] = form.rekey_time.data
 
         #local
         local_params = OrderedDict()
         the_conn['local'] = local_params
 
-        if bool(form.local_round):
-            local_params['local_round'] = form.local_round
-        local_params['auth'] = form.local_auth
-        local_params['id'] = form.local_id
+        if bool(form.local_round.data):
+            local_params['round'] = form.local_round.data
+        local_params['auth'] = form.local_auth.data
+        local_params['id'] = form.local_id.data
 
         print('init form accept C (remote)')
         #remote
         remote_params = OrderedDict()
         the_conn['remote'] = remote_params
-        if bool(form.remote_round):
-            remote_params['remote_round'] = form.remote_round
-        remote_params['auth'] = form.remote_auth
-        remote_params['id'] = form.remote_id
+        if bool(form.remote_round.data):
+            remote_params['round'] = form.remote_round.data
+        remote_params['auth'] = form.remote_auth.data
+        remote_params['id'] = form.remote_id.data
 
 
         #children
@@ -94,57 +94,56 @@ def connection_form():
         the_conn['children'] = children_params
 
         the_child_param = OrderedDict()
-        children_params[form.children_name] = the_child_param
-        the_child_param['esp_proposals'] = form.esp_proposals
-        the_child_param['sha256_96'] = form.sha256_96
-        the_child_param['local_ts'] = form.local_ts
-        the_child_param['remote_ts'] = form.remote_ts
-        if bool(form.child_rekey_time):
-            the_child_param['rekey_time'] = form.child_rekey_time
-        if bool(form.child_lifetime):
-            the_child_param['lifetime'] = form.child_lifetime
-        the_child_param['mode'] = form.child_mode
-        the_child_param['policies'] = form.child_policies
-        the_child_param['policies_fwd_out'] = form.child_policies_fwd_out
-        the_child_param['dpd_action'] = form.dpd_action
-        the_child_param['ipcomp'] = form.ipcomp
-        if bool(form.child_inactivity):
-            the_child_param['inactivity'] = form.child_inactivity
-        if bool(form.child_reqid):
-            the_child_param['reqid'] = form.child_reqid
-        if bool(form.child_priority):
-            the_child_param['priority'] = form.child_priority
-        if bool(form.child_interface):
-            the_child_param['interface'] = form.child_interface
-        if bool(form.mark_in):
-            the_child_param['mark_in'] = form.mark_in
-        if bool(form.mark_in_sa):
-            the_child_param['mark_in_sa'] = form.mark_in_sa
-        if bool(form.mark_out):
-            the_child_param['mark_out'] = form.mark_out
-        if bool(form.set_mark_in):
-            the_child_param['set_mark_in'] = form.set_mark_in
-        if bool(form.set_mark_out):
-            the_child_param['set_mark_out'] = form.set_mark_out
-        if bool(form.if_id_in):
-            the_child_param['if_id_in'] = form.if_id_in
-        if bool(form.if_id_out):
-            the_child_param['if_id_out'] = form.if_id_out
-        if bool(form.child_label):
-            the_child_param['label'] = form.child_label
-        if bool(form.label_mode):
-            the_child_param['label_mode'] = form.label_mode
-        if bool(form.tfc_padding):
-            the_child_param['tfc_padding'] = form.tfc_padding
-        if bool(form.replay_window):
-            the_child_param['replay_window'] = form.replay_window
-        the_child_param['hw_offload'] = form.hw_offload
-        the_child_param['copy_df'] = form.copy_df
-        the_child_param['copy_ecn'] = form.copy_ecn
-        the_child_param['copy_dscp'] = form.copy_dscp
-        the_child_param['start_action'] = form.start_action
-        the_child_param['close_action'] = form.close_action
-        print('init form accept E (children)')
+        children_params[form.children_name.data] = the_child_param
+        the_child_param['esp_proposals'] = form.esp_proposals.data
+        the_child_param['sha256_96'] = form.sha256_96.data
+        the_child_param['local_ts'] = form.local_ts.data
+        the_child_param['remote_ts'] = form.remote_ts.data
+        if bool(form.child_rekey_time.data):
+            the_child_param['rekey_time'] = form.child_rekey_time.data
+        if bool(form.child_lifetime.data):
+            the_child_param['lifetime'] = form.child_lifetime.data
+        the_child_param['mode'] = form.child_mode.data
+        the_child_param['policies'] = form.child_policies.data
+        the_child_param['policies_fwd_out'] = form.child_policies_fwd_out.data
+        the_child_param['dpd_action'] = form.dpd_action.data
+        the_child_param['ipcomp'] = form.ipcomp.data
+        if bool(form.child_inactivity.data):
+            the_child_param['inactivity'] = form.child_inactivity.data
+        if bool(form.child_reqid.data):
+            the_child_param['reqid'] = form.child_reqid.data
+        if bool(form.child_priority.data):
+            the_child_param['priority'] = form.child_priority.data
+        if bool(form.child_interface.data):
+            the_child_param['interface'] = form.child_interface.data
+        if bool(form.mark_in.data):
+            the_child_param['mark_in'] = form.mark_in.data
+        if bool(form.mark_in_sa.data):
+            the_child_param['mark_in_sa'] = form.mark_in_sa.data
+        if bool(form.mark_out.data):
+            the_child_param['mark_out'] = form.mark_out.data
+        if bool(form.set_mark_in.data):
+            the_child_param['set_mark_in'] = form.set_mark_in.data
+        if bool(form.set_mark_out.data):
+            the_child_param['set_mark_out'] = form.set_mark_out.data
+        if bool(form.if_id_in.data):
+            the_child_param['if_id_in'] = form.if_id_in.data
+        if bool(form.if_id_out.data):
+            the_child_param['if_id_out'] = form.if_id_out.data
+        if bool(form.child_label.data):
+            the_child_param['label'] = form.child_label.data
+        if bool(form.label_mode.data):
+            the_child_param['label_mode'] = form.label_mode.data
+        if bool(form.tfc_padding.data):
+            the_child_param['tfc_padding'] = form.tfc_padding.data
+        if bool(form.replay_window.data):
+            the_child_param['replay_window'] = form.replay_window.data
+        the_child_param['hw_offload'] = form.hw_offload.data
+        the_child_param['copy_df'] = form.copy_df.data
+        the_child_param['copy_ecn'] = form.copy_ecn.data
+        the_child_param['copy_dscp'] = form.copy_dscp.data
+        the_child_param['start_action'] = form.start_action.data
+        the_child_param['close_action'] = form.close_action.data
         sess = ViciSess.get_sesssion()
 
         print(sess)
